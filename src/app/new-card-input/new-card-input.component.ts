3d8019efc5b95f4edfb4d05213a26c4a169b30d0
codingkarthik <kartthikspiky@gmail.com>
2018-04-26 19:21:42 +0530

    New Component: new-card-input

---
+++ src/app/new-card-input/new-card-input.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-card-input',
+  templateUrl: './new-card-input.component.html',
+    styleUrls: ['./new-card-input.component.scss'],
+    host: {'class' : 'col-4'}
+})
+export class NewCardInputComponent implements OnInit {
+    public newCard: any = {text:''};
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
